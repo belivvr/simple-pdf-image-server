77e7653d4099b902e1e061d92436ea498a5d6a81
juunini <juuni.ni.i@gmail.com>
2021-11-23 19:02:42 +0900

    Returns PDF page contents

--- src/handlers/ReadPDF.go
+++ src/handlers/ReadPDF.go
@@ -1,20 +1,52 @@
 package handlers
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gen2brain/go-fitz"
 )
 
 func ReadPDF(w http.ResponseWriter, r *http.Request) {
-	doc, err := fitz.New("./files" + r.URL.Path)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+	if r.URL.Path[len(r.URL.Path)-4:] == ".pdf" {
+		numPage, err := getPageNum("./files" + r.URL.Path)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte(fmt.Sprintf("%d", numPage)))
 		return
+	}
+
+	paths := strings.Split(r.URL.Path, "/")
+	pageNumber, _ := strconv.Atoi(paths[len(paths)-1])
+	path := strings.Join(paths[:len(paths)-1], "/")
+	svg, err := getPageSvg("./files"+path, pageNumber)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Write([]byte(svg))
+}
+
+func getPageNum(filepath string) (int, error) {
+	doc, err := fitz.New(filepath)
+	if err != nil {
+		return 0, err
+	}
+	defer doc.Close()
+	return doc.NumPage(), nil
+}
+
+func getPageSvg(filepath string, pageNumber int) (string, error) {
+	doc, err := fitz.New(filepath)
+	if err != nil {
+		return "", err
 	}
 	defer doc.Close()
 
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(docToImageList(doc))
+	svg, err := doc.SVG(pageNumber - 1)
+	return svg, err
 }
